e9e1d5fbbfa155fc7880b2de7f00344e3a18888a
Paolo Rabbito <47228598+paolorabbito@users.noreply.github.com>
2021-12-06 19:09:15 +0100

    Update script.js

--- Day 6/script.js
+++ Day 6/script.js
@@ -2,19 +2,20 @@ const fs = require('fs');
 const data =  fs.readFileSync('input.txt', 'utf-8').split(',');
 castToInt(data);
 
-first_second(data);//Set limit in for loop to 80 day 1 and 256 day2
+first_second(data);
 
 function first_second(data) {
     let fish = [];
     let count = 0;
     fill(data, fish);
     
-    for(i=0; i<80; i++) {
+    for(i=0; i<80; i++) { // 80 for day 1 256 for day 2
         let tmp = fish[0];
 
         for(j=0; j<8; j++) {
             fish[j] = fish[j+1];
         }
+        
         fish[6] += tmp;
         fish[8] = tmp;
     }
@@ -35,7 +36,6 @@ function fill(inp, outp) {
     for(i=0; i<9; i++) 
         outp[i] = 0;
 
-    for(i=0; i<inp.length; i++) {
+    for(i=0; i<inp.length; i++) 
         outp[inp[i]]++;
-    }
 }
